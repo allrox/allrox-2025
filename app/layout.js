7ec63356e13a542885c320af3e09c37327491e58
allrox <alexandre@allrox.com.br>
2024-11-01 01:30:40 -0300

    Inclusão da cor institucional no Body e ajustes do título e descrição da página

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "AllRox",
-  description: "Tecnologia, Marketing e Negócios",
+  title: "AllRox • Tecnologia para Negócios",
+  description: "Soluções personalizadas em tecnologia, transformação digital, desenvolvimento de Sites, Landing Pages, Design de UX, UI, Squads Ágeis e Projetos Personalizados.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-darkBackground">
         {children}
       </body>
     </html>
