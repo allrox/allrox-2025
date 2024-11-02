01eccebb22dc16496431278553da8f556150f396
allrox <alexandre@allrox.com.br>
2024-11-02 18:21:32 -0300

    Inclusão de seção formatada com duas colunas

---
+++ app/Components/Section2Col/index.js
@@ -0,0 +1,13 @@
+import { Children } from "react";
+
+export default function Section2col (props){
+    return(
+<section id="como-funciona" className="pt-16">
+<div className="container mx-auto px-4">
+  <div className="grid grid-cols-1 lg:grid-cols-2 p-8 md:p-16 gap-16 border-solid border-2 border-white/[.05] rounded-xl">
+    {props.children}
+  </div>
+</div>
+</section>
+    )
+}
