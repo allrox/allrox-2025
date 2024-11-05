f5d25177a4063402676079b79938c784412e4338
allrox <alexandre@allrox.com.br>
2024-11-04 22:35:09 -0300

    Inclusão da passagem do texto alt para o elemento img através das props

--- app/Components/GlowingCard/index.js
+++ app/Components/GlowingCard/index.js
@@ -1,13 +1,13 @@
 import Badge from '../Badge';
 import H2Title from '../TitleLevel2';
 
-export default function GlowingCard({ title, badge, image, children, id }) {
+export default function GlowingCard({ title, badge, image, children, id, alt }) {
     return (
         <section id={id} className="container justify-self-center w-fit">
             <div className="flex flex-col items-start border-solid border-2 rounded-xl p-8 md:p-16  bg-[#005ba55e] h-full border-sky-400 shadow-[0_0_30px_0_rgba(21,170,255,0.5)]
         ">
                 {image &&
-                    <img src={image} className="h-80 mx-auto" />}
+                    <img src={image} alt={alt} className="h-80 mx-auto" />}
                 {badge && <Badge text={badge} />}
                 <H2Title>{title}</H2Title>
                 {children}
