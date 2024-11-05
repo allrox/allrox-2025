4972ba25ec7c7c00eca6a26d4ede8a19718c363b
allrox <alexandre@allrox.com.br>
2024-11-04 21:57:11 -0300

    Substituição do menu NextUI por versão simplificada baseada em estado

--- app/Components/Navigation/index.js
+++ app/Components/Navigation/index.js
@@ -1,34 +1,51 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import {Logo} from "./Logo.js";
+import { useState } from "react";
+import { Logo } from "./Logo.js";
 
 export default function Navigation() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <Navbar position="static" className="py-8 text-white mx-auto w-fit">
-      <NavbarBrand>
-        <Logo/>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#sobre">
-            Sobre nós
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#como-funciona">
-            Como funciona?
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#portfolio">
-            Portfolio
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#depoimentos">
-            Opiniões de clientes
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+    <div className="flex flex-col sm:flex-row py-8 text-white mx-auto w-full sm:justify-center items-center">
+      <div>
+        <Logo />
+      </div>
+
+      <button
+        className="text-xl sm:hidden text-white mt-4"
+        onClick={toggleMenu}
+      >
+        ☰
+      </button>
+
+      <div className={`${isOpen ? "block" : "hidden"} sm:flex`}>
+        <ul className="text-xl md:text-base flex flex-col sm:flex-row gap-4 mt-4 sm:mt-0 sm:ml-10 text-center leading-snug" justify="center">
+          <li>
+            <a className="hover:text-[#ffcd48] hover:font-bold" href="#sobre">
+              Sobre nós
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-[#ffcd48] hover:font-bold" href="#como-funciona">
+              Como funciona?
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-[#ffcd48] hover:font-bold" href="#portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-[#ffcd48] hover:font-bold" href="#depoimentos">
+              Opiniões de clientes
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
